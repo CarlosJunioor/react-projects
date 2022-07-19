45fe660e7f74eda2820203e2562b21c5971eb6a1
CarlosJunioor <carlosjunior866@gmail.com>
2022-07-19 18:41:26 +0100

    new project

---
+++ app1/src/components/counter.jsx
@@ -0,0 +1,18 @@
+import * from 'react';
+
+const {useState} = React;
+
+export default function counter() {
+
+    const [counter, setCounter] = useState(0);
+
+    return (
+
+        <div className='counter'>
+            <p>test</p>
+
+            <p>{counter}</p>
+
+        </div>
+    )
+}
