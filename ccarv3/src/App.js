ef75d93e16f9610207239d232b87ab6d5088db0f
CarlosJunioor <carlosjunior866@gmail.com>
2022-07-28 17:07:06 +0100

    fdsg

--- ccarv3/src/App.js
+++ ccarv3/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Menu from './components/Menu'
+import './index.css'
 
 
 function App (){
   return (
     <div>
-
     <Menu />
+    <main>
+      TESTE
+    </main>
+  
     </div>
   )
 }
